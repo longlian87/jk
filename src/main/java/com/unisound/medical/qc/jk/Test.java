c268b349e0a6e39f2001eda087aff570700aa9e7
longlian <156645791@qq.com>
2018-12-06 13:36:23 +0800

    modify test

--- src/main/java/com/unisound/medical/qc/jk/Test.java
+++ src/main/java/com/unisound/medical/qc/jk/Test.java
@@ -1,13 +1,18 @@
 package com.unisound.medical.qc.jk;
 
 
+import java.io.File;
+
 import org.dom4j.DocumentException;
 import org.dom4j.Node;
 
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		printXmlFile("/home/longlian/apps/work/docs");
+	}
+	
+	private static void test1() {
 		try {
 			FastTestDocument document = new FastTestDocument(Test.class.getResourceAsStream("test.xml"));
 			Node nameNode = document.selectSingleNode("//document//name");
@@ -21,6 +26,51 @@ public class Test {
 			System.out.println("/test.xml//document//name value is "+nameNode.getText());
 		} catch (DocumentException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * 打印一个目录（文件）下面的所有xml文档
+	 * @param path
+	 */
+	public static void printXmlFile(String path) {
+		File fpath = new File(path);
+		if(fpath.isFile()) {
+			printXmlFile(fpath);
+			return;
+		}
+		
+		File[] files = fpath.listFiles();
+		if(files == null) {
+			return;
+		}
+		
+		for(File file : files) {
+			printXmlFile(file.getPath());
+		}
+	}
+	
+	/**
+	 * 打印一个xml文档
+	 * @param file
+	 */
+	private static void printXmlFile(File file) {
+		
+		try {
+			FastTestDocument document = new FastTestDocument(file);
+			if(document!=null) {
+				System.out.println("--------------------------"+document.getFilename()+"---------------------------");
+				System.out.println();
+				System.out.println();
+				System.out.println(document.getDocument().asXML());
+				System.out.println();
+				System.out.println();
+				
+			}
+		} catch (DocumentException e) {
+//			e.printStackTrace();
+			System.err.println("exception : " + e.getMessage());
 		}
 	}
 
